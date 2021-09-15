19a949d58ad13f254042dd0020ad9f3092f208a8
mohammad <mh.hsp1377@gmail.com>
2021-09-15 15:50:56 +0430

    discount module/controller/sevice generated +

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import ormconfig from './ormconfig';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(ormconfig),],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
